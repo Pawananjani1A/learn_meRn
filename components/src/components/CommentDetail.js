011379cc2ef432caa51e6e0544d67ff17754dd52
PAWANANJANI KUMAR <pawananjanimth1@gmail.com>
2020-10-25 19:39:36 +0530

    Passing multiple props

--- components/src/components/CommentDetail.js
+++ components/src/components/CommentDetail.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import faker from 'faker';
+
 
 const CommentDetail = (props)=>{
   console.log(props);
     return (
         <div className="comment">
             <a href="/" className="avatar">
-                <img alt="avatar" src={faker.image.avatar()} />
+                <img alt="avatar" src={props.imageSrc} />
             </a>
 
             <div className="content">
@@ -15,10 +15,10 @@ const CommentDetail = (props)=>{
                     </a>
             </div>
             <div className="metadata">
-                <span className="date">Today at 5:00PM</span>
+                <span className="date">{props.timeAgo}</span>
             </div>
             <div className="metadata">
-                Nice blog post!
+                {props.comment}
                 </div>
         </div>
     );
--- components/src/index.js
+++ components/src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import faker from 'faker';
 import CommentDetail from './components/CommentDetail';
 
 const App = ()=>{
@@ -7,12 +8,21 @@ const App = ()=>{
         <div className="ui container comments">
             <CommentDetail
                 author="Sam"
+                timeAgo="Today at 4:45PM"
+                comment="Nice blog"
+                imageSrc={faker.image.avatar()}
             />
             <CommentDetail
                 author="Jane"
+                timeAgo="Today at 4:40PM"
+                comment="Working on ReactJs"
+                imageSrc={faker.image.avatar()}
             />
             <CommentDetail
                 author="Smith"
+                timeAgo="Yesterday at 11:45PM"
+                comment="What's up next ?"
+                imageSrc={faker.image.avatar()}
             />
         </div>
     );
