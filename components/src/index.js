629c2ced9f33932e95e17b0e1442d4b75351438d
PAWANANJANI KUMAR <pawananjanimth1@gmail.com>
2020-10-25 20:16:20 +0530

    Custom childeren component using props.children

--- components/src/index.js
+++ components/src/index.js
@@ -7,25 +7,33 @@ import ApprovalCard from './components/ApprovalCard';
 const App = ()=>{
     return (
         <div className="ui container comments">
-            <CommentDetail
-                author="Sam"
-                timeAgo="Today at 4:45PM"
-                comment="Nice blog"
-                imageSrc={faker.image.avatar()}
-            />
-            <CommentDetail
-                author="Jane"
-                timeAgo="Today at 4:40PM"
-                comment="Working on ReactJs"
-                imageSrc={faker.image.avatar()}
-            />
-            <CommentDetail
-                author="Smith"
-                timeAgo="Yesterday at 11:45PM"
-                comment="What's up next ?"
-                imageSrc={faker.image.avatar()}
-            />
-            <ApprovalCard/>
+        <ApprovalCard>
+                <CommentDetail
+                    author="Sam"
+                    timeAgo="Today at 4:45PM"
+                    comment="Nice blog"
+                    imageSrc={faker.image.avatar()}
+                />
+        </ApprovalCard>
+       
+        <ApprovalCard>
+                <CommentDetail
+                    author="Jane"
+                    timeAgo="Today at 4:40PM"
+                    comment="Working on ReactJs"
+                    imageSrc={faker.image.avatar()}
+                />
+        </ApprovalCard>
+            <ApprovalCard>
+                <CommentDetail
+                    author="Smith"
+                    timeAgo="Yesterday at 11:45PM"
+                    comment="What's up next ?"
+                    imageSrc={faker.image.avatar()}
+                />
+            </ApprovalCard>
+            
+            
         </div>
     );
 }
