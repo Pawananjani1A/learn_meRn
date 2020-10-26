ad2bdbfeb2655804a582e2b1048bf632bede50c8
PAWANANJANI KUMAR <pawananjanimth1@gmail.com>
2020-10-26 17:28:41 +0530

    Initializing state through constructors

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -1,25 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-
-
-// Funtional Component
-/*
-const App = ()=>{
-
-    window.navigator.geolocation.getCurrentPosition(
-        (position) =>{ console.log(position);},
-        (err) =>{ console.log(err);}
-    );
-    return (
-        <div>Hi there!</div>
-    );
-}
-*/
-
-
 // Class-based Component
 class App extends  React.Component{
+
+     constructor(props) {
+        super(props);
+        console.log(props);
+
+        this.state = {latitude:null};
+    }
+
+    // React sys we have to define render!
   render(){
       window.navigator.geolocation.getCurrentPosition(
           (position) => { console.log(position); },
