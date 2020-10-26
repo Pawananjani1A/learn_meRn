7961f7345ed11cd943ad514ad14097fcc68f92f8
PAWANANJANI KUMAR <pawananjanimth1@gmail.com>
2020-10-26 07:45:45 +0530

    Refactoring from functional component to class-based component

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -2,6 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 
+
+// Funtional Component
+/*
 const App = ()=>{
 
     window.navigator.geolocation.getCurrentPosition(
@@ -12,7 +15,21 @@ const App = ()=>{
         <div>Hi there!</div>
     );
 }
+*/
 
+
+// Class-based Component
+class App extends  React.Component{
+  render(){
+      window.navigator.geolocation.getCurrentPosition(
+          (position) => { console.log(position); },
+          (err) => { console.log(err); }
+      );
+      return (
+          <div>Hi there!</div>
+      );
+  }
+}
 
 ReactDOM.render(
     <App/>,
