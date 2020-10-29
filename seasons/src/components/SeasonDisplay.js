7db961c2705d9134d5ebcc539177a07788ac6dec
PAWANANJANI KUMAR <pawananjanimth1@gmail.com>
2020-10-29 09:15:31 +0530

    Extracting icons to config object

--- seasons/src/components/SeasonDisplay.js
+++ seasons/src/components/SeasonDisplay.js
@@ -1,6 +1,20 @@
 import React from 'react';
 
 
+
+const seasonConfig = {
+
+    Summer:{
+        text: 'Let\'s hits the beach',
+        iconName:'sun'
+        
+    },
+    Winter:{
+        text:'Burr, it\'s chilly !',
+        iconName:'snowflake'
+    }
+};
+
 const getSeason = (latitude,month)=>{
 
     if(month>2 && month<9)
@@ -14,14 +28,14 @@ const SeasonDisplay = (props)=>{
 //    console.log(props);
 
 const season = getSeason(props.latitude,new Date().getMonth());
-    const text = season === 'Winter' ? 'Burr, it is chilly' : 'Lets hit the beach';
-    const icon = season === 'Winter'?'snowflake':'sun';
+
+    const { text, iconName } = seasonConfig[season];
 
     return(
         <div>
-           <i className={`${icon} icon`}/>
+            <i className={`icon-left massive ${iconName} icon`}/>
             <h1>{text}</h1>
-            <i className={`${icon} icon`} />
+            <i className={`icon-right massive ${iconName} icon`} />
         </div>
     );
 };
