6e3b4b1418e55db60966231f566c16663293ff79
PAWANANJANI KUMAR <pawananjanimth1@gmail.com>
2020-10-30 16:04:34 +0530

    Rendering list of elements

--- pics/src/components/ImageList.js
+++ pics/src/components/ImageList.js
@@ -3,11 +3,15 @@ import React from 'react';
 
 const ImageList = (props)=>{
 
-    console.log(props.images);
+    // console.log(props.images);
+
+    const images = props.images.map((image)=>{
+        return (<img src={image.urls.regular}/>);
+    });
 
     return(
         <div>
-            ImageList
+            {images}
         </div>
     );
 }
