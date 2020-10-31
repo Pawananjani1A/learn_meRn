d6ba439ff589a08e1ea702766014750931536b47
PAWANANJANI KUMAR <pawananjanimth1@gmail.com>
2020-10-31 12:25:47 +0530

    callbacks on image load to make dynamic spans on images

--- pics/src/components/ImageCard.js
+++ pics/src/components/ImageCard.js
@@ -7,13 +7,22 @@ class ImageCard extends React.Component{
 
     constructor(props){
         super(props);
+        
+        this.state = ({spans:0});
 
         this.imageRef = React.createRef();
     }
 
     componentDidMount(){
-       console.log(this.imageRef);
-        console.log(this.imageRef.current.clientHeight);
+       this.imageRef.current.addEventListener('load',this.setSpans);
+    }
+
+    setSpans= ()=>{
+        // console.log(this.imageRef.current.clientHeight);
+        const height = this.imageRef.current.clientHeight;
+
+        const spans = Math.ceil(height/10);
+        this.setState({spans});
     }
 
     render(){
@@ -21,7 +30,7 @@ class ImageCard extends React.Component{
         const {description,urls} = this.props.image;
 
         return (
-            <div>
+            <div style={{gridRowEnd:`span ${this.state.spans}`}}>
                 <img 
                 ref={this.imageRef}
                 alt={description}
