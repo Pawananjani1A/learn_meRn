a54de028c844abb5152cae2b2647a261d0f10dcf
PAWANANJANI KUMAR <pawananjanimth1@gmail.com>
2020-11-02 13:49:20 +0530

    Displaying video player using iframe tag

--- videos/src/components/VideoDetail.js
+++ videos/src/components/VideoDetail.js
@@ -14,8 +14,13 @@ const VideoDetail = (props)=>{
         );
     }
 
+    const videoSrc = `https://www.youtube.com/embed/${video.id.videoId}`;
+
     return (
         <div>
+            <div className="ui embed">
+                <iframe src={videoSrc} title={video.snippet.title}/>
+            </div>
             <div className="ui segment">
                 <h4 className="ui header">{video.snippet.title}</h4>
                 <p className="ui description">{video.snippet.description}</p>
