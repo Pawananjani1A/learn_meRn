3c3b49ee2df4a59dee9623a845775ff1142ef605
PAWANANJANI KUMAR <pawananjanimth1@gmail.com>
2020-11-04 13:29:23 +0530

    Using helper functions inside funactional component

--- widgets/src/components/Accordion.js
+++ widgets/src/components/Accordion.js
@@ -5,11 +5,18 @@ const Accordion = (props)=>{
 
     const {items} = props;
 
-    const renderedItems = items.map((item)=>{
+    const onTitleClick = (index)=>{
+        console.log('Title Clicked', index);
+    };
+
+    const renderedItems = items.map((item,index)=>{
 
         return(
             <React.Fragment key={item.title}>
-                <div className="title active">
+                <div
+                 className="title active"
+                 onClick={()=>onTitleClick(index)}
+                 >
                  <i className="dropdown icon"></i>
                  {item.title}
                 </div>
