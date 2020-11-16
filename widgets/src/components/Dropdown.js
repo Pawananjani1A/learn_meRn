279b6e9766e1b26f0be7c0361f85664119f1942f
PAWANANJANI KUMAR <pawananjanimth1@gmail.com>
2020-11-16 16:55:40 +0530

    A lot of JSX

--- widgets/src/components/Dropdown.js
+++ widgets/src/components/Dropdown.js
@@ -2,9 +2,30 @@ import React from 'react';
 
 
 
-const Dropdown = ()=>{
+const Dropdown = (props)=>{
+
+    const {options} = props;
+     
+    const renderedOptions = options.map((option)=>{
+         return (
+             <div key={option.value} className="item">
+               {option.label}
+             </div>
+         );
+    });
     return (
-        <div>Dropdown</div>
+        <div className="ui form">
+            <div className="field">
+                <label className="label">Select a Color</label>
+                <div className="ui selection dropdown visible active">
+                    <i className="dropdown icon"></i>
+                    <div className="text">Select Color</div>
+                    <div className="menu visible transition">
+                        {renderedOptions}
+                    </div>
+                </div>
+            </div>
+        </div>
     );
 }
 
