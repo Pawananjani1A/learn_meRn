fbe965c4d6e286b0fc3668bc1a3323a1ab045638
PAWANANJANI KUMAR <pawananjanimth1@gmail.com>
2020-11-16 09:41:38 +0530

    setTimeout and useEffect's cleanup function

--- widgets/src/components/Search.js
+++ widgets/src/components/Search.js
@@ -13,25 +13,34 @@ const Search = ()=>{
 
    useEffect(() => {
       
-    const search = async()=>{
-        const {data} = await axios.get('https://en.wikipedia.org/w/api.php',{
-            params:{
-                action:'query',
-                list:'search',
-                origin:'*',
-                format:'json',
-                srsearch:term
+//     const search = async()=>{
+//         const {data} = await axios.get('https://en.wikipedia.org/w/api.php',{
+//             params:{
+//                 action:'query',
+//                 list:'search',
+//                 origin:'*',
+//                 format:'json',
+//                 srsearch:term
 
-            }
-        });
+//             }
+//         });
 
-        setResults(data.query.search);
-    };
+//         setResults(data.query.search);
+//     };
 
-    if(term) search();
-   },[term]);
+//     setTimeout(() => {
+//         if (term) search();
+//     }, 500);
 
+   console.log('Initial render or term was changed.');
 
+   return ()=>{
+       console.log('CLEANUP');
+   };
+
+ },[term]);
+
+    
    const renderedResults = results.map((result)=>{
 
     return (
