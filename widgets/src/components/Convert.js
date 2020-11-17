5dd1c3f1c54d49959f80882c4740bb73f0ef1fed
PAWANANJANI KUMAR <pawananjanimth1@gmail.com>
2020-11-17 16:31:00 +0530

    Displaying translated text

--- widgets/src/components/Convert.js
+++ widgets/src/components/Convert.js
@@ -1,24 +1,34 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 
 
 const Convert = (props)=>{
   
     const {language,text} = props;
+    const [translated,setTranslated] = useState('');
 
     useEffect(()=>{
         // console.log("Language or text changed!");
-        axios.post('https://translation.googleapis.com/language/translate/v2',{},{
-            params: {
-                q:text,
-                target:language.value,
-                key: 'AIzaSyCHUCmpR7cT_yDFHC98CZJy2LTms-IwDlM'    
-            }
-        });
+        const doTranslation = async ()=>{
+            const response = await axios.post('https://translation.googleapis.com/language/translate/v2', {}, {
+                params: {
+                    q: text,
+                    target: language.value,
+                    key: 'AIzaSyCHUCmpR7cT_yDFHC98CZJy2LTms-IwDlM'
+                }
+            });
+
+            const { data } = response;
+            setTranslated(data.data.translations[0].translatedText);
+        };
+
+       doTranslation();
     },[language,text]);
 
     return (
-        <div></div>
+        <div>
+            <h1 className="ui header">{translated}</h1>
+        </div>
     );
 }
 
