8342dde5ca6fd28becbb8313e52d6ee8ecf8357a
PAWANANJANI KUMAR <pawananjanimth1@gmail.com>
2020-11-18 10:43:50 +0530

    Basic component Routing

--- widgets/src/components/App.js
+++ widgets/src/components/App.js
@@ -35,17 +35,40 @@ const options = [
     }
 ];
 
+const showAccordion = ()=>{
+   if(window.location.pathname==='/'){
+       return <Accordion items={items} /> ;
+   }
+};
+
+const showList = ()=>{
+    if(window.location.pathname==='/list') return <Search/> ;
+};
+
+const showDropdown = ()=>{
+    if(window.location.pathname==='/dropdown') 
+        return <Dropdown 
+                  label="Select a Color"
+                  options={options}
+                  selected={options[0]}
+                  
+                  />
+                                                
+};
+
+const showTranslate = ()=>{
+    if(window.location.pathname==='/translate')
+     return <Translate/>;
+}
+
 const App =  ()=>{
      
     const [selected,setSelected] = useState(options[0]);
     
     return(
         <div className="ui container">
-        
-        <Translate/>
-          
-            
-
+        {showAccordion()}
+        {showList()}
         </div>
     );
 };
