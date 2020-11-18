3e295ea6e8490f0090e960780a994f76a1727924
PAWANANJANI KUMAR <pawananjanimth1@gmail.com>
2020-11-18 12:08:11 +0530

    Detecting URL change from Route component

--- widgets/src/components/Link.js
+++ widgets/src/components/Link.js
@@ -7,8 +7,14 @@ const Link = (props)=>{
     const {className,href,children} = props;
 
     const onClick = (event)=>{
+        //This line prevents full-page reload on changing the URL
          event.preventDefault();
+         //This line changes the URL without full-page reload
          window.history.pushState({},'',href);
+
+         //These two lines communicate to the route component that the URL has changed
+         const navEvent = new PopStateEvent('popstate');
+         window.dispatchEvent(navEvent);
     };
     
 
