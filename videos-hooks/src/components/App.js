b16c004a28354bd5446fdc4ff6c47881a4aff95c
PAWANANJANI KUMAR <pawananjanimth1@gmail.com>
2020-11-20 11:20:11 +0530

    Refactoring the App component from class-based to functional

--- videos-hooks/src/components/App.js
+++ videos-hooks/src/components/App.js
@@ -1,69 +1,64 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import SearchBar from './SearchBar';
 import YouTube from '../apis/YouTube';
 import VideoList from './VideoList';
 import VideoDetail from './VideoDetail';
 
+const App = ()=>{
+   
+    const [videos,setVideos] = useState([]);
+    const [selectedVideo,setSelectedVideo] = useState(null);
 
-class App extends React.Component{
-
-   state =  {videos:[], selectedVideo:null};
-
-
-   componentDidMount(){
-       this.onTermSubmit('nodejs');
-   }
-
-   onTermSubmit = async (term)=>{
-    //    console.log(term);
-
-      const response = await YouTube.get("/search", {
-          params:{
-              q:term
-          }
-       });
-
-    //    console.log(response);
-
-    this.setState({
-        videos:response.data.items,
-        selectedVideo:response.data.items[0]
-    });
-
-   }
-
-   onVideoSelect = (video)=>{
-    //    console.log('From the app!',video);
-    this.setState({selectedVideo:video});
-   };
+     useEffect(()=>{
+         onTermSubmit('DP algorithms');
+     },[]);
 
 
-    render(){
-        return (
-            <div className="ui container">
-              <SearchBar 
-               onFormSubmit = {this.onTermSubmit}
-              /> 
-              Found {this.state.videos.length } videos.
-              <div className="ui grid">
-              <div className="ui row">
-              <div className="eleven wide column">
-                            <VideoDetail
-                                video={this.state.selectedVideo}
-                            />
-              </div>
-                      <div className="five wide column">
-                            <VideoList
-                                videos={this.state.videos}
-                                onVideoSelect={this.onVideoSelect}
-                            />
-                      </div>  
-              </div>
-              </div>
+    const onTermSubmit = async (term) => {
+        //    console.log(term);
+
+        const response = await YouTube.get("/search", {
+            params: {
+                q: term
+            }
+        });
+
+        //    console.log(response);
+         setVideos(response.data.items);
+        setSelectedVideo(response.data.items[0]);
+        
+    };
+
+   
+
+    return (
+        <div className="ui container">
+            <SearchBar
+                onFormSubmit={onTermSubmit}
+            />
+              Found {videos.length} videos.
+            <div className="ui grid">
+                <div className="ui row">
+                    <div className="eleven wide column">
+                        <VideoDetail
+                            video={selectedVideo}
+                        />
+                    </div>
+                    <div className="five wide column">
+                        <VideoList
+                            videos={videos}
+                            onVideoSelect={setSelectedVideo}
+                        />
+                    </div>
+                </div>
             </div>
-        );
-    }
-}
+        </div>
+    );
+};
+
+
+
+
 
 
 export default App;
