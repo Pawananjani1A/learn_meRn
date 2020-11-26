87c92957277e9012c67acc1cb93dd55e566deb3f
PAWANANJANI KUMAR <pawananjanimth1@gmail.com>
2020-11-26 19:30:12 +0530

    Configuring connet to mapStateToProps and building songList

--- songs/src/components/SongList.js
+++ songs/src/components/SongList.js
@@ -5,20 +5,41 @@ import {connect} from 'react-redux';
 
 class SongList extends React.Component{
 
-
+  renderList()
+  {
+      return this.props.songs.map((song)=>{
+         
+        return (
+             <div className="ui item" key={song.title}>
+                 <div className="right floated content">
+                     <button className="ui button primary">Select</button>
+                 </div>
+                 <div className="content">{song.title}</div>
+             </div>
+        );
+      });
+  }
 
 
 
 
     render()
-    {
+    {  
+        console.log(this.props);
         return (
-            <div>
-                songList
+            <div className="ui divided list">
+               {this.renderList()}
             </div>
         );
     }
 };
 
+const mapStateToProps = (state)=>{
 
-export default connect()(SongList);
+    // console.log(state);
+
+    return {songs:state.songs};
+};
+
+
+export default connect(mapStateToProps)(SongList);
