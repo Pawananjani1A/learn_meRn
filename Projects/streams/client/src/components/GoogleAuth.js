82d0067474a31319251fa2305dec33233987a9e6
PAWANANJANI KUMAR <pawananjanimth1@gmail.com>
2020-12-18 19:22:46 +0530

    Redux Basic SetUp

--- Projects/streams/client/src/components/GoogleAuth.js
+++ Projects/streams/client/src/components/GoogleAuth.js
@@ -29,11 +29,11 @@ class GoogleAuth extends React.Component{
     this.setState({isSignedIn:this.auth.isSignedIn.get()});
  }
 
- onSignIn = ()=>{
+ onSignInClick = ()=>{
   this.auth.signIn();
  };
 
- onSignOut = ()=>{
+ onSignOutClick = ()=>{
   this.auth.signOut();
  };
 
@@ -46,7 +46,7 @@ class GoogleAuth extends React.Component{
      else if(this.state.isSignedIn)
      {
          return (
-             <button className="ui red google button" onClick={this.onSignOut}>
+             <button className="ui red google button" onClick={this.onSignOutClick}>
              <i className="google icon"/>
                  Sign Out
              </button>
@@ -55,7 +55,7 @@ class GoogleAuth extends React.Component{
      else
      {
          return (
-             <button className="ui red google button" onClick={this.onSignIn}>
+             <button className="ui red google button" onClick={this.onSignInClick}>
                  <i className="google icon"/>
                  Sign In with Google
              </button>
