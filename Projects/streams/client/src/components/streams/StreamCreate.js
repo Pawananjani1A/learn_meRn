e4c085de7ccfa2620f0d6397a119f4a48ee7426b
PAWANANJANI KUMAR <pawananjanimth1@gmail.com>
2020-12-20 18:30:18 +0530

    Automatically handling form-input events through reduxForm props

--- Projects/streams/client/src/components/streams/StreamCreate.js
+++ Projects/streams/client/src/components/streams/StreamCreate.js
@@ -4,15 +4,24 @@ import {Field,reduxForm} from 'redux-form';
 
 class StreamCreate extends React.Component{
 
-
+renderInput(formProps)
+{ 
+    // console.log(formProps);
+    const {input} = formProps;
+  return (
+      <div>
+      <input {...input}/>
+      </div>
+  );
+}
 
 
     render(){
         console.log(this.props);
         return (
             <form>
-                <Field name="title"/>
-                <Field name="description"/>
+                <Field name="title" component={this.renderInput}/>
+                <Field name="description" component={this.renderInput}/>
             </form>
         );
     }
