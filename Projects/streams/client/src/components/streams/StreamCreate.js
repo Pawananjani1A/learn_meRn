7f54fdfbd0f4a1f1963dee305a61800f81341a95
PAWANANJANI KUMAR <pawananjanimth1@gmail.com>
2020-12-20 18:14:22 +0530

    Creating forms

--- Projects/streams/client/src/components/streams/StreamCreate.js
+++ Projects/streams/client/src/components/streams/StreamCreate.js
@@ -1,14 +1,25 @@
 import React from 'react';
+import {Field,reduxForm} from 'redux-form';
+
+
+class StreamCreate extends React.Component{
 
 
 
-const StreamCreate = () => {
 
-    return (
-        <div>StreamCreate</div>
-    );
+    render(){
+        console.log(this.props);
+        return (
+            <form>
+                <Field name="title"/>
+                <Field name="description"/>
+            </form>
+        );
+    }
 };
 
 
 
-export default StreamCreate;
+export default reduxForm({
+    form:'streamCreate'
+}) (StreamCreate);
