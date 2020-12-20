94939e9e8d24666ac704debc97a065476a9fd95d
PAWANANJANI KUMAR <pawananjanimth1@gmail.com>
2020-12-20 19:01:45 +0530

    Handling form Submission

--- Projects/streams/client/src/components/streams/StreamCreate.js
+++ Projects/streams/client/src/components/streams/StreamCreate.js
@@ -9,21 +9,29 @@ renderInput(formProps)
     // console.log(formProps);
     const {input,label} = formProps;
   return (
-      <div className="ui container">
+      <div>
       <label>{label}</label>
       <input {...input}/>
       </div>
   );
 }
 
+onSubmit(formValues)
+{
+    console.log(formValues);
+}
+
 
     render(){
-        console.log(this.props);
+        // console.log(this.props);
         return (
-            <form className="ui form">
-                <Field name="title" component={this.renderInput} label="Title"/>
-                <Field name="description" component={this.renderInput} label="Description"/>
-            </form>
+            <div className="ui container">
+                <form className="ui form" onSubmit={this.props.handleSubmit(this.onSubmit)}>
+                    <Field name="title" component={this.renderInput} label="Title" />
+                    <Field name="description" component={this.renderInput} label="Description" />
+                    <button className="ui button primary">Submit</button>
+                </form>
+            </div>
         );
     }
 };
