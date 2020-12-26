5b21d8e26fb403bdc0044149ec5e05c5b5453835
PAWANANJANI KUMAR <pawananjanimth1@gmail.com>
2020-12-26 19:25:41 +0530

    Coding  all action creators in one go

--- Projects/streams/client/src/actions/index.js
+++ Projects/streams/client/src/actions/index.js
@@ -1,5 +1,13 @@
 import { formValues } from 'redux-form';
-import {SIGN_IN,SIGN_OUT,CREATE_STREAM} from './types';
+import {
+    SIGN_IN,
+    SIGN_OUT,
+    CREATE_STREAM,
+    FETCH_STREAM,
+    FETCH_STREAMS,
+    DELETE_STREAM,
+    EDIT_STREAM
+} from './types';
 import streams from '../apis/streams';
 
 
@@ -31,6 +39,44 @@ export const createStream = (formValues)=>{
             payload: response.data
         });
     });
+};
 
+export const fetchStreams = ()=> async dispatch => {
 
+    const response = await streams.get('/streams');
+
+    dispatch({
+        type:FETCH_STREAMS,
+        payload: response.data
+    });
+};
+
+export const fetchStream = (id) => async dispatch => {
+   
+    const response = await streams.get(`/streams/${id}`);
+     
+    dispatch({
+        type:FETCH_STREAM,
+        payload: response.data
+    });
+};
+
+export const editStream = (id,formValues) => async dispatch =>{
+
+    const response = streams.put(`/streams/${id}`,formValues);
+
+    dispatch({
+        type:EDIT_STREAM,
+        payload: response.data
+    });
+};
+
+export const deleteStream = (id)=>async dispatch =>{
+   
+    await streams.delete(`/streams/${id}`);
+
+    dispatch({
+        type:DELETE_STREAM,
+        payload:id
+    });
 };
