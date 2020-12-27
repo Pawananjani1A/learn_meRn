ca6b2453971efa037b6ea8ede5851e814a11c93a
PAWANANJANI KUMAR <pawananjanimth1@gmail.com>
2020-12-27 17:03:28 +0530

    Fetching an object of streams in streamReducer and combining streamReducer output with combineReducers in our state

--- Projects/streams/client/src/reducers/index.js
+++ Projects/streams/client/src/reducers/index.js
@@ -2,9 +2,11 @@ import {combineReducers} from 'redux';
 import {reducer as formReducer} from 'redux-form';
 
 import authReducer from './authReducer';
+import streamReducer from './streamReducer';
 
 export default combineReducers({
     authReducer:authReducer,
     //Acessing the pre-built reducer from redux-form
-    form:formReducer
+    form:formReducer,
+    streams:streamReducer
 });
--- Projects/streams/client/src/reducers/streamReducer.js
+++ Projects/streams/client/src/reducers/streamReducer.js
@@ -10,10 +10,15 @@ import {
 
 
 
+//FETCH_STREAMS has a little bit of lodash syntax to convert the incoming array of streams to object of streams
+//Again, DELETE_STREAM has some lodash syntax as well to return the state without the element having id==action.payload
+//All other cases just update the state with the incoming stream = action.payload and then return the state.
+
 const streamReducer = (state={},action)=>{
   
     switch (action.type)
-    {   
+    {   case FETCH_STREAMS:
+            return {...state,..._.mapKeys(action.payload,'id')};
         case FETCH_STREAM:
             return {...state,[action.payload.id]:action.payload};
         case CREATE_STREAM:
