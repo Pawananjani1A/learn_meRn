c1fcd3c43aff70ad0cd5d9a673e4914d1225b9ca
PAWANANJANI KUMAR <pawananjanimth1@gmail.com>
2020-12-27 17:23:42 +0530

    Rendering the streamList

--- Projects/streams/client/src/components/streams/StreamList.js
+++ Projects/streams/client/src/components/streams/StreamList.js
@@ -9,18 +9,39 @@ class StreamList extends React.Component{
         this.props.fetchStreams();
         }
 
-    render() {
+    renderList()
+    {
+        return this.props.streams.map(stream=>{
+           return (
+               <div className="item" key={stream.id}>
+                  <i className="large middle aligned icon camera"/>
+                  <div className="content">
+                       {stream.title}
+                       <div className="description">
+                         {stream.description}
+                       </div>
+                  </div>
+               </div>
+           );
+        });
+    }
 
+    render() {
+    //   console.log(this.props.streams);
         return (
             <div>
-                StreamList
+            <h2>Streams</h2>
+            <div className="ui celled list">
+                 {this.renderList()}
+            </div>
             </div>
         );
     };
 }
 
 const mapStateToProps = (state)=>{
-
+   
+    return {streams:Object.values(state.streams)};
 };
 
-export default connect(null,{fetchStreams})(StreamList);
+export default connect(mapStateToProps,{fetchStreams})(StreamList);
