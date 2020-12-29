3559dc23d9ec5283d883aecf70f919476844dee7
PAWANANJANI KUMAR <pawananjanimth1@gmail.com>
2020-12-29 16:16:52 +0530

    Refactoring the StreamEdit component and passing initialValues props to the StreamForm component

--- Projects/streams/client/src/components/streams/StreamEdit.js
+++ Projects/streams/client/src/components/streams/StreamEdit.js
@@ -2,7 +2,8 @@ import React from 'react';
 import {connect} from 'react-redux';
 
 
-import {fetchStream} from '../../actions';
+import {fetchStream,editStream} from '../../actions';
+import StreamForm from './StreamForm';
 
 class StreamEdit extends React.Component {
 
@@ -10,7 +11,11 @@ componentDidMount()
 {
     this.props.fetchStream(this.props.match.params.id);
 }
+  
 
+  onSubmit = (formValues)=>{
+      console.log(formValues);
+  }
 
     render() {
         // console.log(this.props);
@@ -27,7 +32,13 @@ componentDidMount()
         }
         else {
             return (
-                <div>{this.props.stream.title}</div>
+                <div>
+                    <h3>Edit a Stream</h3>
+                    <StreamForm 
+                    initialValues={{ title: `${this.props.stream.title}`, description: `${this.props.stream.description}`}}
+                    onSubmit={this.onSubmit}
+                    />
+                </div>
             );
         }
     }
@@ -39,4 +50,4 @@ const mapStateToProps= (state,ownProps)=>{
 return {stream:state.streams[ownProps.match.params.id]};
 };
 
-export default connect(mapStateToProps,{fetchStream}) (StreamEdit);
+export default connect(mapStateToProps,{fetchStream,editStream}) (StreamEdit);
