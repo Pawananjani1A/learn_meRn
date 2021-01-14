b0b3aef7aae69c228fe16f2e05bca68215c0f171
PAWANANJANI KUMAR <pawananjanimth1@gmail.com>
2021-01-14 16:18:27 +0530

    Fetching the required stream fron our state

--- Projects/streams/client/src/components/streams/StreamShow.js
+++ Projects/streams/client/src/components/streams/StreamShow.js
@@ -1,14 +1,47 @@
 import React from 'react';
+import {connect} from 'react-redux';
+import {fetchStream} from '../../actions';
+
+class StreamShow extends React.Component {
 
 
+  componentDidMount(){
+    //   console.log(this.props);
+    this.props.fetchStream(this.props.match.params.id);
+  }
 
-const StreamShow = () => {
 
-    return ( 
-    <div > StreamShow </div>
-    );
+    render()
+    {  
+         // console.log(this.props.stream);
+        if(!this.props.stream)
+        {
+            return (
+                <div className="ui segment">
+                    <div className="ui active inverted dimmer">
+                        <div className="ui text loader">Fetching the Form</div>
+                    </div>
+                    <p></p>
+                </div>
+            );
+        }
+        else
+         {  
+             const {title,description} = this.props.stream;
+             return (
+            <div>
+            <h2>{title}</h2>
+            <h5>{description}</h5>
+            </div>
+        );
+        }
+    }
+}
+
+
+const mapStateToProps = (state,ownProps)=>{
+
+    return {stream:state.streams[ownProps.match.params.id]};
 };
 
-
-
-export default StreamShow;
+export default connect(mapStateToProps,{fetchStream}) (StreamShow);
