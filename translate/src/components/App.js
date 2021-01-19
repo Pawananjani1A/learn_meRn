82c85b629e4583d6c57e8509a1b4652388840f67
PAWANANJANI KUMAR <pawananjanimth1@gmail.com>
2021-01-19 17:21:41 +0530

    Updating context with change in state and pushing it inside the component pipe in which we need it

--- translate/src/components/App.js
+++ translate/src/components/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 
 import UserCreate from './UserCreate';
+import LanguageContext from '../contexts/LanguageContext';
+
 
 class App extends React.Component{
  
@@ -19,7 +21,9 @@ class App extends React.Component{
                <i className="flag us" onClick={()=>this.onLanguageChange('english')}/>
                <i className="flag in" onClick={()=>this.onLanguageChange('hindi')}/>
            </div>
-           <UserCreate/>
+           <LanguageContext.Provider value={this.state.language}>
+               <UserCreate/>
+           </LanguageContext.Provider>
             </div>
         );
     }
