76765b952c34e956e24662c9810ab1392a1cde85
PAWANANJANI KUMAR <pawananjanimth1@gmail.com>
2021-01-19 16:11:37 +0530

    Writing the App component to render language as state

--- translate/src/components/App.js
+++ translate/src/components/App.js
@@ -3,12 +3,24 @@ import React from 'react';
 
 
 class App extends React.Component{
+ 
+    state = {language:'english'};
 
-
+    //We've to make sure this is an arrow function as we're passing it to something else as a callback function
+    onLanguageChange = (language)=>{
+        this.setState({language});
+    };
 
     render() {
         return (
-            <div className="ui container">App</div>
+            <div className="ui container">
+           <div>
+               Select a Language : 
+               <i className="flag us" onClick={()=>this.onLanguageChange('english')}/>
+               <i className="flag in" onClick={()=>this.onLanguageChange('hindi')}/>
+           </div>
+           {this.state.language}
+            </div>
         );
     }
 }
