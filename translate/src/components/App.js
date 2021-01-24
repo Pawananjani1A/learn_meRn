47d4c7acfd063842f7f3482d329b323362f22b3b
PAWANANJANI KUMAR <pawananjanimth1@gmail.com>
2021-01-24 17:38:41 +0530

    Rendering the LanguageStore with all the buisness logic inside itself only

--- translate/src/components/App.js
+++ translate/src/components/App.js
@@ -1,30 +1,27 @@
 import React from 'react';
 
 import UserCreate from './UserCreate';
-import LanguageContext from '../contexts/LanguageContext';
+import {LanguageStore} from '../contexts/LanguageContext';
 import ColorContext from '../contexts/ColorContext';
 import LanguageSelector from './LanguageSelector';
 class App extends React.Component{
  
-    state = {language:'english'};
-
-    //We've to make sure this is an arrow function as we're passing it to something else as a callback function
-    onLanguageChange = (language)=>{
-        this.setState({language});
-    };
+    
 
     
     render() {
-       const buttonColor =  this.state.language==='english'?"primary":"red";
+       
         return (
             <div className="ui container">
-           <LanguageSelector onLanguageChange={this.onLanguageChange}/>
+           <LanguageStore>
+               <LanguageSelector/>
            
-           <ColorContext.Provider value={buttonColor}>
-           <LanguageContext.Provider value={this.state.language}>
+           <ColorContext.Provider value="red">
+           
                <UserCreate/>
-           </LanguageContext.Provider>
+           
            </ColorContext.Provider>
+           </LanguageStore>
            
             </div>
         );
